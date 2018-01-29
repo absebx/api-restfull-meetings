8b86dbb8cbbb688f8e8a93a01017a89f04c7facb
asder707 <abn.sebick@gmail.com>
2018-01-29 18:14:04 -0300

    test production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = require('./app');
 
-var port = process.env.port || 4500;
+var port = process.env.port /*|| 4500*/;
 var dbLocal = 'mongodb://localhost:27017/app_meetings';
 var dbmLab = 'mongodb://abner:asder24682468@ds117888.mlab.com:17888/app_meetings';
 mongoose.connect(dbmLab, (err, res)=>{
